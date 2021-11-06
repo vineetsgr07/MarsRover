b2e4da7e762dca8cb1769d7b2833c40ef966a866
vineet sagar <vineetsagar@vineets-MacBook-Pro.local>
2021-11-06 21:04:46 +0530

    Added mars rover movement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-const {readFromText} = require('./utils')
-const { MarsRover } = require('./marsRover')
+const { readFromText } = require('./utils')
+const { Rover } = require('./MarsRover')
 
 readFromText().then((data) => {
-    console.log(data)
+    const result = new Map()
+
+    for (let i = 0; i < data.length; i++) {
+        const { grid, position, instruction } = transformArguments(data[i])
+        const rover = Rover({
+            x: position[0],
+            y: position[1],
+            direction: position[2],
+            grid: {
+                width: grid[0],
+                height: grid[1]
+            }
+        })
+
+        instruction.forEach((action) => rover.move(action));
+        result.set(i, {
+            position: rover.getPosition(),
+            direction: rover.getDirection()
+        })
+    }
+
+    printToConsole(result)
 })
 
-module.exports = {
-    processInput
+const transformArguments = (data) => {
+    return {
+        grid: data.grid
+            .split(" ")
+            .map((item) => parseInt(item)),
+        position: data.position
+            .split(" ")
+            .map((item, index) => [0, 1]
+                .includes(index) ? parseInt(item) : item),
+        instruction: data.instructions.split("")
+    }
+}
+
+
+const printToConsole = (result) => {
+    for (let index = 0; index < result.size; index++) {
+        console.log(`Rover${index + 1}:${result.get(index).position.x} ${result.get(index).position.y} ${result.get(index).direction}`)
+    }
 }
--- src/marsRover.js
+++ src/marsRover.js
@@ -1,12 +1,65 @@
-function MarsRover({
-    grid,
-    position,
-    instruction
-}) {
-    console.log(grid, position, instructions)
+'use strict';
+
+const Rover = ({
+    x,
+    y,
+    direction,
+    grid
+}) => {
+    let _direction = direction;
+    let _x = x;
+    let _y = y;
+    const directions = {
+        N: { left: 'W', right: 'E', move: () => _y++ },
+        S: { left: 'E', right: 'W', move: () => _y-- },
+        E: { left: 'N', right: 'S', move: () => _x++ },
+        W: { left: 'S', right: 'N', move: () => _x-- }
+    };
+
+    const getDirection = () => _direction;
+    const turnLeft = () => _direction = directions[_direction].left
+    const turnRight = () => _direction = directions[_direction].right;
+    const moveForward = () => directions[_direction].move.call(this);
+    const validateRover = (x, y) => x >= 0 && y >= 0 && x <= grid.width && y <= grid.height
+
+    const getPosition = () => {
+        return {
+            x: _x, y: _y
+        }
+    };
+
+    const projectMove = () => {
+        var x = _x;
+        var y = _y;
+        directions[_direction].move;
+        var projectedPosition = getPosition();
+        _x = x;
+        _y = y;
+        return projectedPosition;
+    };
+
+    const move = (action) => {
+        if (action === 'L') {
+            turnLeft();
+        } else if (action === 'R') {
+            turnRight();
+        } else if (action === 'M') {
+            var projectedMove = projectMove();
+            if (validateRover(projectedMove.x, projectedMove.y)) {
+                moveForward();
+            } else {
+                throw new Error('invalid Position of rover');
+            }
+        }
+    };
+
+    return {
+        move: move,
+        getPosition: getPosition,
+        getDirection: getDirection
+    }
 }
 
-
 module.exports = {
-    MarsRover: MarsRover
+    Rover: Rover
 }
